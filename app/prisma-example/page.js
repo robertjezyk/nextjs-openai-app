5585edb1deb1b8efde202ac1dc8d7463cd3d15c3
robertjezyk <rjezyk@vp.pl>
2023-12-21 16:25:22 +0000

    going to prod

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,12 +1,36 @@
-import { TaskList } from "@/components/TaskList";
+import prisma from "@/utils/db";
 
-const PrismaExamplePage = async () => {
-  return (
-    <>
-      <h1 className="text-7xl mb-8">Prisma Example Page</h1>
-      <TaskList />
-    </>
-  );
+const prismaHandlers = async () => {
+  console.log("prisma example");
+  // await prisma.task.create({
+  //   data: {
+  //     content: 'wake up'
+  //   }
+  // });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
 };
 
+const PrismaExamplePage = async () => {
+  const tasks = await prismaHandlers();
+  if (tasks.length === 0) {
+    return <h2 className="mt-8 font-medium text-lg">No tasks to show...</h2>;
+  }
+  return (
+    <div>
+      <h1 className="text-3xl font-medium mb-4">PrismaExamplePage</h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-xl py-2">
+            {task.content}
+          </h2>
+        );
+      })}
+    </div>
+  );
+};
 export default PrismaExamplePage;
